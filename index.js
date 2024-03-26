92f0f01c91d2bc9f392471e91b68b01b2f0a6ecd
wendymasese <wendy.masese@student.moringaschool.com>
2024-03-26 17:07:16 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if(ride <= 400) {
+    return "This one is on me!"
+  }else if(ride >= 400 &&ride<= 2000) {
+    return "That will be twenty bucks.";
+  }else if (ride > 2000 && ride <=2500) {
+    return "I will gladly take your thirty bucks.";
+  }else {
+    return "No can do.";
+  }
+
+ 
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good.";
+  }else  {
+    return "No go.";
+  }
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
+ 
 
-function switchOnCharmFromTip(){
+
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+
+  }
 }
